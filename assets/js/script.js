9cc636f8e7c3a8167764450e8ef65fbdd7b1ef76
alcirock7 <147304763+alcirock7@users.noreply.github.com>
2026-08-28 21:06:46 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,9 +59,9 @@ function goToCheckout(event, artId) {
             'exclusive': 'https://siten.lemonsqueezy.com/checkout/buy/link12'
         },
         'art5': {
-            'standard': 'https://infinite-galleria.lemonsqueezy.com/checkout/buy/d23a29de-e684-48e8-8993-749089bdc644?desc=0&discount=0',
-            'extended': 'https://infinite-galleria.lemonsqueezy.com/checkout/buy/d23a29de-e684-48e8-8993-749089bdc644?desc=0&discount=0',
-            'exclusive': 'https://infinite-galleria.lemonsqueezy.com/checkout/buy/d23a29de-e684-48e8-8993-749089bdc644?desc=0&discount=0'
+            'standard': 'https://infinite-galleria.lemonsqueezy.com/checkout/buy/d23a29de-e684-48e8-8993-749089bdc644?discount=0',
+            'extended': 'https://infinite-galleria.lemonsqueezy.com/checkout/buy/d23a29de-e684-48e8-8993-749089bdc644?discount=0',
+            'exclusive': 'https://infinite-galleria.lemonsqueezy.com/checkout/buy/d23a29de-e684-48e8-8993-749089bdc644?discount=0'
         },
         'art6': {
             'standard': 'https://siten.lemonsqueezy.com/checkout/buy/link16',
